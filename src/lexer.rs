fc50c5bbabf1fcabe147a0bf56e094f92335d841
Steyn van Litsenborgh <steyn.van.l@gmail.com>
2022-06-18 19:46:48 +0200

    lexer: Added module and Token enum

---
+++ src/lexer.rs
@@ -0,0 +1,66 @@
+pub enum Token {
+    // end-of-file
+    Eof,
+    // identifier
+    Id(String),
+    // number literal
+    Number(i32),
+    // string literal
+    String(String),
+
+    // keywords
+    Array,
+    Begin,
+    Call,
+    Do,
+    Else,
+    Elsif,
+    End,
+    False,
+    Function,
+    Get,
+    If,
+    Integer,
+    Leave,
+    Not,
+    Put,
+    Relax,
+    Source,
+    Then,
+    To,
+    True,
+    While,
+
+    // relational operators
+    Equal,
+    GreaterEqual,
+    GreaterThan,
+    LessEqual,
+    LessThan,
+    NotEqual,
+
+    // additive operators
+    Minus,
+    Or,
+    Plus,
+
+    // multiplicative operators
+    And,
+    Divide,
+    Multiply,
+    Remainder,
+
+    /* other non-alphabetic operators */
+    CloseBracket,
+    CloseParenthesis,
+    Comma,
+    Concatenate,
+    Gets,
+    OpenBracket,
+    OpenParenthesis,
+    Semicolon,
+}
+
+pub fn lex() -> Vec<Token> {
+    vec![Token::Eof]
+}
